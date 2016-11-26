689fba72691d76b57eb85d28e747bff590f78603
nguyennhuan127 <nguyennhuan127@gmail.com>
2016-11-26 09:53:22 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://thietkethiconghoboi.com'>http://thietkethiconghoboi.com"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY CLICK VAO DAY : <a href='http://asagroup.vn'>http://asagroup.vn"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
